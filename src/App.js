e12b97cbd630a335b82b26245886fedac36d6bd9
ben-kovie <benedict.yekovie@gmail.com>
2022-09-12 15:42:20 +0100

    react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", color: "blue" }}> 
+       our project</h1>
+    </div>
+  )
+}
+
+export default App
+
