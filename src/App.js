cbb0b479f29305e8dd8fe9c080ff7419991ed47f
ben-kovie <benedict.yekovie@gmail.com>
2022-09-18 21:58:44 +0100

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,47 +1,45 @@
 import React from 'react'
-<<<<<<< HEAD
 import { NavBar} from './component'
 // import { Account} from './component'
 import { RoyalCourtNotices} from './component'
 // import { History } from './component'
-// import TheGinuwa from './component/pages/TheGinuwa/TheGinuwa'
-// import LandingPage from './component/pages/LandingPage/LandingPage'
-
+import TheGinuwa from './component/pages/TheGinuwa/TheGinuwa'
+ import LandingPage from './component/pages/LandingPage/LandingPage'
+import { Route, Routes} from "react-router-dom"
 import { PageTemplate } from './component'
+import {HistoryPage} from './component'
 
-function App() {
-  return (<>
-     <PageTemplate header={<NavBar />}>
+
+
+
+
+     
       {/* <History /> */}
       {/* <Account /> */}
-      <RoyalCourtNotices />
+      
       {/* <TheGinuwa/> */}
       {/* <LandingPage/> */}
-     </PageTemplate>
-    </>
-=======
-import { Route, Routes} from "react-router-dom"
-import {HistoryPage} from './component'
-//import { PageTemplate } from './component'
-import LandingPage from './component/pages/LandingPage/LandingPage'
-import TheGinuwa from './component/pages/TheGinuwa/TheGinuwa'
+     
+  
+
 
 function App() {
   return (
   // <PageTemplate header= {}>
    //   <HistoryPage />
    <div>
-   
+   <PageTemplate header={<NavBar />}>
      <Routes>
       <Route path ='/' element = {<LandingPage/>}/>
+      <Route path ='/royalCourtNotices' element ={<RoyalCourtNotices />}/>
        <Route path ='/historPage' element = {<HistoryPage/>}/>
           <Route path ='/theGinuwa' element = {<TheGinuwa/>}/>
          </Routes>
-       
+       </PageTemplate>
           </div>
 
  //    </PageTemplate>
->>>>>>> main
+
   )
 }
 
